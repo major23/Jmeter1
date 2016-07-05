f715360667fb569686efedc4141177ddb5f40116
Nikos <nikos.alexiou@upstreamsystems.com>
2016-07-05 17:37:55 +0300

    fghfhf

--- src/main/java/org/attune/RunJemerTests.java
+++ src/main/java/org/attune/RunJemerTests.java
@@ -1,51 +1,69 @@
 package org.attune;
+import org.apache.commons.io.FileUtils;
 import org.apache.jmeter.engine.StandardJMeterEngine;
 import org.apache.jmeter.save.SaveService;
 import org.apache.jmeter.util.JMeterUtils;
 import org.apache.jorphan.collections.HashTree;
+
+import java.io.File;
 import java.io.FileInputStream;
+import java.util.Collection;
+import java.util.Iterator;
 
 public class RunJemerTests {
 
-    public static void main(String[] argv) throws Exception {
 
-    }
-    
-    public int checkPer()
+    public int checkPer(String file)
     {
     	try
     	{
-        // JMeter Engine
-        StandardJMeterEngine jmeter = new StandardJMeterEngine();
-
-        System.out.println(1);
-        // Initialize Properties, logging, locale, etc.
-        JMeterUtils.loadJMeterProperties("F:/JMeter/apache-jmeter-3.0/bin/jmeter.properties");
-        JMeterUtils.setJMeterHome("F:/JMeter/apache-jmeter-3.0");
-        JMeterUtils.initLogging();// you can comment this line out to see extra log messages of i.e. DEBUG level
-        JMeterUtils.initLocale();
-
-        // Initialize JMeter SaveService
-        SaveService.loadProperties();
-        System.out.println(2);
-
-
-        // Load existing .jmx Test Plan
-        FileInputStream in = new FileInputStream("src/test/jmeter/AttueUSers3.jmx");
-        HashTree testPlanTree = SaveService.loadTree(in);
-        in.close();
-
-        // Run JMeter Test
-        jmeter.configure(testPlanTree);
-        System.out.println(3);
-        jmeter.run();
-        System.out.println(4);
-    	return 1;
+	        // JMeter Engine
+	        StandardJMeterEngine jmeter = new StandardJMeterEngine();
+	        // Initialize Properties, logging, locale, etc.
+	        JMeterUtils.loadJMeterProperties("/home/nikos/Downloads/jmeter/apache-jmeter-3.0/bin/jmeter.properties");
+	        JMeterUtils.setJMeterHome("/home/nikos/Downloads/jmeter/apache-jmeter-3.0");
+	        JMeterUtils.initLogging();// you can comment this line out to see extra log messages of i.e. DEBUG level
+	        JMeterUtils.initLocale();
+	
+	        // Initialize JMeter SaveService
+	        SaveService.loadProperties();
+	
+	        // Load existing .jmx Test Plan
+//	        FileInputStream in = new FileInputStream("src/test/jmeter/AttueUSers3.jmx");
+//	        HashTree testPlanTree = SaveService.loadTree(in);
+//	        in.close();
+	        
+	        File jmxFile = new File(find(file));//      "/src/test/jmeter/Test1.jmx");
+	        HashTree testPlanTree = SaveService.loadTree(jmxFile);
+	        
+	       
+	
+	        // Run JMeter Test
+	        jmeter.configure(testPlanTree);
+	        System.out.println("Running jmeter test: "+jmxFile);
+	        jmeter.run();
+	        Thread.sleep(2000);
+	    	return 1;
     	}
     	catch(Exception e)
     	{
     		return 0;
     	} 
-    	}
+    }
+    
+    
+	@SuppressWarnings("rawtypes")
+	public String find(String fileName) throws Exception {
+	    File root = new File(System.getProperty("user.dir"));
+	    boolean recursive = true;
+	    Collection files = FileUtils.listFiles(root, null, recursive);
+	    for (Iterator iterator = files.iterator(); iterator.hasNext();) {
+	    	File file = (File) iterator.next();
+	        if(file.getName().equals(fileName)){
+	        	return file.getAbsolutePath();
+	        }
+	    }
+	    return fileName;
+	} 
     
 }
--- src/test/java/te/SampleTestCaseTest.java
+++ src/test/java/te/SampleTestCaseTest.java
@@ -3,20 +3,17 @@ package te;
 
 import org.attune.RunJemerTests;
 import org.testng.Assert;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class SampleTestCaseTest {
 
-  @Test
-  public void MyTest1() {
-	  int a=10;
-		Assert.assertEquals(10, a);
-  }
-
-  @Test
-  public void PerTest() {
-	  RunJemerTests ob=new RunJemerTests();
-		int a= ob.checkPer();
+	@Parameters({"jmxFile"})
+	@Test
+	public void PerTest(@Optional String file) {
+		RunJemerTests ob=new RunJemerTests();
+		int a= ob.checkPer(file);
 		System.out.println("value of a   "+a);
 		Assert.assertEquals(1, a);
   }
